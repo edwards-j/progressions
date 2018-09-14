5ccc5c520481b20a8d82867f794b1fa1e24f01ab
Jonathan Edwards <jonathan_edwards@Jonathans-MacBook-Pro.local>
2018-09-14 13:44:52 -0500

    got tilt working on dashboard buttons and song list cards

--- src/components/PublicSongs/PublicSongCard.js
+++ src/components/PublicSongs/PublicSongCard.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import '../YourSongs/SongCard.css'
+import Tilt from 'react-tilt'
 
 export default class SongCard extends Component {
     componentDidMount() {
         this.props.getAllUsers()
     }
-render() {
-    return (
-        <div className="song-card">
-            <div className="has-background-info card-title">
-                <h5 className="is-size-5 has-text-white">{this.props.song.title}</h5>
-            </div>
-            <div className="card-body">
-                <p className="card-description">{this.props.song.description}</p>
-                <hr />
-                <p className="is-italic is-size-7 has-text-grey">Composer: {this.props.song.username}</p>
-                <div className="has-text-centered">
-                    <button className="button is-small is-rounded is-outlined"><Link to={`/public-songs/${this.props.song.id}`}>Details</Link></button>
+    render() {
+        return (
+            <Tilt className="Tilt song-card" options={{ reverse: true, max: 20 }} >
+                <div className="Tilt-inner">
+                        <div className="card-title">
+                            <h5 className="is-size-5 has-text-white">{this.props.song.title}</h5>
+                        </div>
+                        <div className="card-body">
+                            <p className="card-description">{this.props.song.description}</p>
+                            <hr />
+                            <p className="is-italic is-size-7 has-text-grey">Composer: {this.props.song.username}</p>
+                            <div className="has-text-centered">
+                                <button className="button is-small is-rounded is-outlined"><Link to={`/public-songs/${this.props.song.id}`}>Details</Link></button>
+                            </div>
+                        </div>
                 </div>
-            </div>
-        </div>
-    )
-}
+            </Tilt>
+        )
+    }
 }
